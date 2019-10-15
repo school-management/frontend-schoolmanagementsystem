67007393ecc2c706669fd95be84a5f60b2da3534
kiishkishan <kishanth001@gmail.com>
2019-10-15 18:18:14 +0530

    basic layout : sidenav and topbar finished but have to to search input

---
+++ src/components/layout/FlexContainer.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import theme from '../../theme/theme';
+
+const FlexContainer = styled.div`
+    background: ${theme.colors.base};
+    display: flex;
+`
+
+export default FlexContainer;
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,14 +1,22 @@
 import styled from 'styled-components';
 import theme from '../../theme/theme';
+import Navigator from './Navigator';
 
 
 const Sidebar = styled.div`
     background: #5d4f8d;
     width: 180px;
-    height: 568px;
+    height: 625px;
     text-align: center;
     color: ${theme.colors.white};
-    font-family: ${theme.font.fontFamily}
+    font-family: ${theme.font.fontFamily};
+    box-shadow: ${theme.size.btnBoxShadow1};
+    border-right: 0.5em;
+    display: flex;
+    flex-direction: column;
+
+
+
    
 
 `
