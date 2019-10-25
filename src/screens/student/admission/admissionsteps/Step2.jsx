4cd5d962c98ce49768da1cb211574e5b50fb3174
pakeesan <sanpakees@gmail.com>
2019-10-25 11:59:47 +0530

    minor changess

--- src/screens/student/admission/admissionsteps/Step2.jsx
+++ src/screens/student/admission/admissionsteps/Step2.jsx
@@ -24,115 +24,115 @@ export default class Step2 extends Component {
     render() {
         return (
             <FormContainer>
-                <form>
-                    <FormRow>
-                        <FormColumn>
-                            <Label>Father Name</Label>
-                            <FlatInput placeholder="Enter your Father Name"></FlatInput>
 
-                        </FormColumn>
+                <FormRow>
+                    <FormColumn>
+                        <Label>Father Name</Label>
+                        <FlatInput placeholder="Enter your Father Name"></FlatInput>
 
-                        <FormColumn>
-                            <Label>Father NIC No</Label>
-                            <FlatInput placeholder="Father NIC No"></FlatInput>
-                        </FormColumn>
+                    </FormColumn>
 
-
-                        <FormColumn>
-                            <Label>Father Occupation :</Label>
-                            <FlatInput placeholder="Father Occupation "></FlatInput>
-                        </FormColumn>
-
-                    </FormRow>
-                    <FormRow>
-                        <FormColumn>
-                            <Label>Father Moblie No:</Label>
-                            <FlatInput placeholder="Father Moblie No"></FlatInput>
-
-                        </FormColumn>
+                    <FormColumn>
+                        <Label>Father NIC No</Label>
+                        <FlatInput placeholder="Father NIC No"></FlatInput>
+                    </FormColumn>
 
 
-                        <FormColumn>
-                            <Label>Father Office Address :</Label>
-                            <FlatInput placeholder="Father Office Address"></FlatInput>
-                        </FormColumn>
+                    <FormColumn>
+                        <Label>Father Occupation :</Label>
+                        <FlatInput placeholder="Father Occupation "></FlatInput>
+                    </FormColumn>
+
+                </FormRow>
+                <FormRow>
+                    <FormColumn>
+                        <Label>Father Moblie No:</Label>
+                        <FlatInput placeholder="Father Moblie No"></FlatInput>
+
+                    </FormColumn>
+
+
+                    <FormColumn>
+                        <Label>Father Office Address :</Label>
+                        <FlatInput placeholder="Father Office Address"></FlatInput>
+                    </FormColumn>
 
 
 
-                        <FormColumn>
-                            <Label>Father Email:</Label>
-                            <FlatInput placeholder="Father Email"></FlatInput>
-                        </FormColumn>
-                    </FormRow>
-                    <FormRow>
-                        <FormColumn>
-                            <Label>Father Old Student :</Label>
-                            <Select style={{ height: "10px" }} options={options} />
-                        </FormColumn>
-                        <FormColumn>
-                            <Label>From:</Label>
+                    <FormColumn>
+                        <Label>Father Email:</Label>
+                        <FlatInput placeholder="Father Email"></FlatInput>
+                    </FormColumn>
+                </FormRow>
+                <FormRow>
+                    <FormColumn>
+                        <Label>Father Old Student :</Label>
+                        <Select style={{ height: "10px" }} options={options} />
+                    </FormColumn>
+                    <FormColumn>
+                        <FormRow><Label>From:</Label>
                             <FlatInput placeholder="From"></FlatInput>
-                        </FormColumn>
-                        <FormColumn>
                             <Label>To :</Label>
                             <FlatInput placeholder="To"></FlatInput>
-                        </FormColumn>
-                    </FormRow>
-                    <FormRow>
-                        <FormColumn>
-                            <Label>Mother Name :</Label>
-                            <FlatInput placeholder="Mother Name"></FlatInput>
+                        </FormRow>
+                    </FormColumn>
+                    <FormColumn>
 
-                        </FormColumn>
+                    </FormColumn>
+                </FormRow>
+                <FormRow>
+                    <FormColumn>
+                        <Label>Mother Name :</Label>
+                        <FlatInput placeholder="Mother Name"></FlatInput>
 
-                        <FormColumn>
-                            <Label>Mother Occupation :</Label>
-                            <FlatInput placeholder="Mother Occupation"></FlatInput>
-                        </FormColumn>
-                        <FormColumn>
-                            <Label>Mother Office Address :</Label>
-                            <FlatInput placeholder="Mother Office Address "></FlatInput>
-                        </FormColumn>
-                    </FormRow>
-                    <FormRow>
-                        <FormColumn>
-                            <Label>Mother Mobile No:</Label>
-                            <FlatInput placeholder="Mother Mobile No"></FlatInput>
-                        </FormColumn>
-                        <FormColumn>
-                            <Label>Mother Email :</Label> <FlatInput placeholder="Mother Email"></FlatInput>
-                        </FormColumn>
-                        <FormColumn>
-                            <Label>Mother Old Student:</Label> <Select style={{ height: "10px" }} options={options} />
-                        </FormColumn>
-                    </FormRow>
-                    <FormRow>
-                        <FormColumn>
-                            <Label>From:</Label> <FlatInput placeholder="From"></FlatInput>
-                        </FormColumn>
-                        <FormColumn>
-                            <Label>To :</Label> <FlatInput placeholder="To"></FlatInput>
-                        </FormColumn>
-                        <FormColumn>
-                            <Label>Guardian Name :</Label> <FlatInput placeholder="Guardian Name"></FlatInput>
-                        </FormColumn>
-                    </FormRow>
-                    <FormRow>
-                        <FormColumn>
-                            <Label>Guardian Occupation:</Label> <FlatInput placeholder="Guardian Occupation"></FlatInput>
-                        </FormColumn>
-                    </FormRow>
-                    <FormRow>
-                        <FormColumn>
-                            <Label>Guardian Moblie No:</Label> <FlatInput placeholder="Guardian Moblie No"></FlatInput>
-                        </FormColumn>
-                    </FormRow>
-                    <FormRow>
-                        <FormColumn>
-                            <Label>Guardian Email:</Label> <FlatInput placeholder="Guardian Email"></FlatInput>
-                        </FormColumn>
-                    </FormRow>
-                </form>
+                    </FormColumn>
+
+                    <FormColumn>
+                        <Label>Mother Occupation :</Label>
+                        <FlatInput placeholder="Mother Occupation"></FlatInput>
+                    </FormColumn>
+                    <FormColumn>
+                        <Label>Mother Office Address :</Label>
+                        <FlatInput placeholder="Mother Office Address "></FlatInput>
+                    </FormColumn>
+                </FormRow>
+                <FormRow>
+                    <FormColumn>
+                        <Label>Mother Mobile No:</Label>
+                        <FlatInput placeholder="Mother Mobile No"></FlatInput>
+                    </FormColumn>
+                    <FormColumn>
+                        <Label>Mother Email :</Label> <FlatInput placeholder="Mother Email"></FlatInput>
+                    </FormColumn>
+                    <FormColumn>
+                        <Label>Mother Old Student:</Label> <Select style={{ height: "10px" }} options={options} />
+                    </FormColumn>
+                </FormRow>
+                <FormRow>
+                    <FormColumn>
+                        <Label>From:</Label> <FlatInput placeholder="From"></FlatInput>
+                    </FormColumn>
+                    <FormColumn>
+                        <Label>To :</Label> <FlatInput placeholder="To"></FlatInput>
+                    </FormColumn>
+                    <FormColumn>
+                        <Label>Guardian Name :</Label> <FlatInput placeholder="Guardian Name"></FlatInput>
+                    </FormColumn>
+                </FormRow>
+                <FormRow>
+                    <FormColumn>
+                        <Label>Guardian Occupation:</Label> <FlatInput placeholder="Guardian Occupation"></FlatInput>
+                    </FormColumn>
+
+                    <FormColumn>
+                        <Label>Guardian Moblie No:</Label> <FlatInput placeholder="Guardian Moblie No"></FlatInput>
+                    </FormColumn>
+
+                    <FormColumn>
+                        <Label>Guardian Email:</Label> <FlatInput placeholder="Guardian Email"></FlatInput>
+                    </FormColumn>
+                </FormRow>
+
             </FormContainer >
         )
     }
