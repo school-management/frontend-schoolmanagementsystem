1a94d2fd4ace0e89173819debe6390b32d41098e
pakeesan <sanpakees@gmail.com>
2019-10-29 15:45:47 +0530

    resolved

--- src/screens/student/admission/admissionsteps/ParentForm.jsx
+++ src/screens/student/admission/admissionsteps/ParentForm.jsx
@@ -25,6 +25,18 @@ const isOldStudent = [
     { value: "false", label: 'No' }
 ]
 
+const Year = [
+    { value: "2000", label: '2000' },
+    { value: "2001", label: '2001' },
+    { value: "2002", label: '2002' },
+    { value: "2003", label: '2003' },
+    { value: "2004", label: '2004' },
+    { value: "2005", label: '2005' },
+    { value: "2006", label: '2006' },
+    { value: "2007", label: '2007' }
+]
+
+
 class ParentForm extends Component {
 
     state = {
